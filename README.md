245e5e5875374f26212cc249809182d9416be71a
Sean Morris <sean@seanmorr.is>
2019-12-31 07:45:58 -0500

    Initial.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-README.md
+# dotsyntax-sublime
+
+*Highlight your dotfiles your way.*
+
+I'm tired of having to deal with a lack of syntax highlighting when creating a custom dotfile. Even worse, I cannot stand having to configure each and every editor to use the correct standard.
+
+This plugin changes all that. Simply place a `.syntax` file in the root of any project and sublime will automatically use it to highlight the files in that project the rules you define.
+
+A syntaxfile is simple a `key:value` map, where the keys are filenames or extensions to act on, and the values are the file types that you want to use. For example, the syntaxfile below will cause`.syntax` to be highlighted as if it were a `.yml` file, and the .env file to be renderd like bash.
+
+Please note, this does not cause these files to become `yml` or `bash` files, it only configures the syntax highglighter to ease eye strain.
+
+```yaml
+.syntax: .yaml
+.env:    .bash
+```
---
+++ dotsyntax.py
@@ -0,0 +1,117 @@
+import os.path
+import sublime
+import sublime_plugin
+import yaml
+import io
+import re
+import pprint
+
+yaml.add_multi_constructor('tag:yaml.org,2002:value', lambda loader, suffix, node: None)
+
+class DotSyntaxCommand(sublime_plugin.EventListener):
+
+	mappings = {}
+	
+	def on_load_async(self, view):
+
+		self.set_syntax(view)
+
+	def on_post_save_async(self, view):
+
+		self.set_syntax(view)
+
+	def set_syntax(self, view):
+
+		for _view in view.window().views():
+			file        = _view.file_name();
+			mapped_ext  = self.map_file_extension(file)
+			syntax_file = self.lookup_syntax_file(mapped_ext)
+
+			print(mapped_ext)
+			print(syntax_file)
+
+			_view.settings().set('syntax', syntax_file)
+
+	def lookup_syntax_file(self, extension):
+
+		if(len(self.mappings) == 0):
+
+			self.load_settings()
+
+		if extension in self.mappings:
+
+			return self.mappings[extension]
+	
+	def map_file_extension(self, file):
+		
+		*_ ,ext = os.path.splitext(file)
+
+		filename = os.path.basename(file)
+
+		dir  = os.path.dirname(file)
+
+		dot_syntax_file = self.find_dotsyntax_file(dir)
+
+		if dot_syntax_file:
+			
+			with open(dot_syntax_file, "r") as handle:
+
+				defs = [i.strip().rpartition(':') for i in handle]
+				match = next((i for i in defs if i[0].strip() in {ext,filename}), None)
+	
+				if not match:
+					return None
+
+				*_, extension = match
+
+				return extension.strip()
+
+	def find_dotsyntax_file(self, dir):
+
+		while dir != '/':
+
+			syntaxFile = dir + '/.syntax'
+
+			if os.path.exists(syntaxFile):
+
+				return syntaxFile
+
+			dir = os.path.abspath(os.path.join(dir, os.pardir))
+
+	def load_settings(self):
+
+		syntaxDefFiles = sublime.find_resources('*.sublime-syntax')
+
+		extmode=0
+
+		for syntaxDefFile in syntaxDefFiles:
+
+			try:
+
+				settings = sublime.load_resource(syntaxDefFile)
+
+				for line in io.StringIO(settings):
+
+					if extmode == 1:
+
+						match = re.match('\s{2}-(.+?)\s+?\#?', line)
+
+						if match is not None:	
+
+							extension = '.' + match.group(1).strip()
+							self.mappings[extension] = syntaxDefFile
+
+						else:
+
+							extmode = 0
+
+					match = re.match('^file_extensions\:', line)
+					
+					if match is not None:
+					
+						extmode=1
+						continue;
+
+			except:
+
+				continue
